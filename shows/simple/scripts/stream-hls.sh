2f8f5d8053c42c19d1f0bb7bc98c2965b890859c
Joe <joerbowers@gmail.com>
2021-10-14 13:28:11 -0700

    Stream with sound

--- shows/simple/scripts/stream-hls.sh
+++ shows/simple/scripts/stream-hls.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-rm ./stream/*
+rm ./stream/* && true
 
 ./shows/simple/target/release/simple | \
-    ffmpeg -f h264 -i pipe: -f hls -hls_time 6 ./stream/simple.m3u8
+    ffmpeg -f h264 -i pipe: \
+        -stream_loop -1 -i ./shows/simple/media/design-for-dreaming.mp3 \
+        -f hls -hls_time 6 ./stream/simple.m3u8
--- shows/simple/scripts/stream-rtmp.sh
+++ shows/simple/scripts/stream-rtmp.sh
@@ -2,4 +2,7 @@
 
 . $(pwd)/SECRETS
 
-./shows/simple/target/release/simple | ffmpeg -loglevel debug -f h264 -i pipe: -f flv -vcodec libx264 -g 30 "$RTMP_INGEST"
+./shows/simple/target/release/simple | \
+    ffmpeg -loglevel debug -f h264 -i pipe: \
+        -stream_loop -1 -i ./shows/simple/media/design-for-dreaming.mp3 \
+        -f flv -vcodec libx264 -g 30 "$RTMP_INGEST"
