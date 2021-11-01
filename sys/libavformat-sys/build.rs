35e5cf767490a94eebe671142e12ea83563cf758
Joe <joerbowers@gmail.com>
2021-11-01 10:22:27 -0700

    work around where libavformat violates bindgen assumptions
    In particular, libavformat defines macros for some C++ math constants,
    and bindgen has a quirk around 128bit floats in C.

--- sys/libavformat-sys/build.rs
+++ sys/libavformat-sys/build.rs
@@ -1,5 +1,24 @@
+use bindgen::callbacks::{MacroParsingBehavior, ParseCallbacks};
 use std::env;
 use std::path::PathBuf;
+
+// Apparently, some library in our supply chain redefines the constants
+// from cmath as macros (which TBH seems fine) but it breaks bindgen
+// unless we do something about it.
+
+#[derive(Clone, Copy, Debug)]
+struct OmitMathMacros {}
+
+impl ParseCallbacks for OmitMathMacros {
+    fn will_parse_macro(&self, name: &str) -> MacroParsingBehavior {
+        match name {
+            "FP_NAN" | "FP_INFINITE" | "FP_ZERO" | "FP_SUBNORMAL" | "FP_NORMAL" => {
+                MacroParsingBehavior::Ignore
+            }
+            _ => MacroParsingBehavior::Default,
+        }
+    }
+}
 
 fn main() {
     println!("cargo:rustc-link-lib=avformat");
@@ -8,6 +27,7 @@ fn main() {
     let bindings = bindgen::Builder::default()
         .header("include/wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(OmitMathMacros {}))
         .generate()
         .expect("Unable to generate bindings");
 
